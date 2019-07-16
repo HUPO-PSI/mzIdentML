37ad42753e3c7f59837d7a6a8587bb8243993f43
Gerhard Mayer <gerhard64mayer@gmx.de>
2019-07-16 13:12:29 +0200

    Updated run.bat

--- validator/trunk/run.bat
+++ validator/trunk/run.bat
@@ -4,10 +4,7 @@ set JAVA_HOME=C:\Programs\openjdk-12.0.1_windows-x64_bin\jdk-12.0.1\bin\
 
 %JAVA_HOME%java.exe -version
 
-REM for Java 6/7/8/11/12
+REM for Java 11/12
 %JAVA_HOME%java.exe %JAVA_OPTS% -cp .\lib -jar .\mzIdentMLValidator-1.4.35-SNAPSHOT.jar
-
-REM for Java 9/10
-REM java.exe %JAVA_OPTS% --add-modules java.xml.bind -cp .\lib -jar .\mzIdentMLValidator-1.4.35-SNAPSHOT.jar
 
 pause
